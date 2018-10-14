9fe476d9cb11a645d2ab45d6aaaf81833a766b7c
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-10-15 00:33:51 +0900

    add jp resource

--- app/src/main/java/com/kazu_cocoa/appbundlesample/MainActivity.kt
+++ app/src/main/java/com/kazu_cocoa/appbundlesample/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.kazu_cocoa.appbundlesample
 
+import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +33,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentLocale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            resources.configuration.locales.get(0)
+        } else {
+            resources.configuration.locale
+        }
+        Log.d(this.javaClass.simpleName, "current locale is: $currentLocale")
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
